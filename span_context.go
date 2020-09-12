4b52234000102ba27bb45e9d5335dfe9d275bb3e
Gleb Dubrovin <termiusone@gmail.com>
2020-09-12 21:18:17 +0300

    add description to LogKV

--- span_context.go
+++ span_context.go
@@ -12,6 +12,24 @@ func SpanFromContext(ctx context.Context) opentracing.Span {
 	return opentracing.SpanFromContext(ctx)
 }
 
+// LogKV is a concise, readable way to record key:value logging data about
+// a Span, though unfortunately this also makes it less efficient and less
+// type-safe than LogFields(). Here's an example:
+//
+//    span.LogKV(
+//        "event", "soft error",
+//        "type", "cache timeout",
+//        "waited.millis", 1500)
+//
+// For LogKV (as opposed to LogFields()), the parameters must appear as
+// key-value pairs, like
+//
+//    span.LogKV(key1, val1, key2, val2, key3, val3, ...)
+//
+// The keys must all be strings. The values may be strings, numeric types,
+// bools, Go error instances, or arbitrary structs.
+//
+// (Note to implementors: consider the log.InterleavedKVToFields() helper)
 func LogKV(ctx context.Context, key string, value interface{}) {
 	if span := SpanFromContext(ctx); span != nil {
 		span.LogKV(key, value)
