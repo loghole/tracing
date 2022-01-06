95241e13511d1cd5fcaad7f3e2fcbd241c1bf33a
lissteron <lissteron@gmail.com>
2022-01-06 18:48:09 +0300

    fix

---
+++ internal/spanprocessor/spanprocessor.go
@@ -0,0 +1,194 @@
+package spanprocessor
+
+import (
+	"context"
+	"sync"
+
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type Sampled struct {
+	mu sync.Mutex
+
+	sampler   tracesdk.Sampler
+	processor tracesdk.SpanProcessor
+
+	traces map[trace.TraceID]*traceWrapper
+}
+
+func NewSampled(
+	processor tracesdk.SpanProcessor,
+	sampler tracesdk.Sampler,
+) *Sampled {
+	sampledProcessor := &Sampled{
+		processor: processor,
+		sampler:   sampler,
+		traces:    map[trace.TraceID]*traceWrapper{},
+	}
+
+	return sampledProcessor
+}
+
+func (p *Sampled) OnStart(parent context.Context, span tracesdk.ReadWriteSpan) {
+	p.onStart(parent, span)
+	p.processor.OnStart(parent, span)
+}
+
+func (p *Sampled) OnEnd(s tracesdk.ReadOnlySpan) {
+	tr, ok := p.getTrace(s.SpanContext().TraceID())
+	if !ok {
+		return
+	}
+
+	spanID := s.SpanContext().SpanID()
+
+	if !tr.isFinished && !tr.isParent(spanID) {
+		return
+	}
+
+	hasError := tr.hasError()
+
+	if tr.isParent(spanID) {
+		tr.isFinished = true
+
+		for _, span := range tr.extractFinishedSpans() {
+			p.send(span.context, span.span, hasError)
+		}
+
+		return
+	}
+
+	if spanWrapper, ok := tr.spans[spanID]; ok {
+		p.send(spanWrapper.context, s, hasError)
+	}
+}
+
+func (p *Sampled) Shutdown(ctx context.Context) error {
+	p.flush()
+
+	return p.processor.Shutdown(ctx)
+}
+
+func (p *Sampled) ForceFlush(ctx context.Context) error {
+	p.flush()
+
+	return p.processor.ForceFlush(ctx)
+}
+
+func (p *Sampled) onStart(parent context.Context, span tracesdk.ReadWriteSpan) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	spanCtx := span.SpanContext()
+
+	if val, ok := p.traces[spanCtx.TraceID()]; ok {
+		val.storeSpan(parent, span)
+	} else {
+		p.traces[spanCtx.TraceID()] = &traceWrapper{
+			spans: map[trace.SpanID]spanWrapper{
+				spanCtx.SpanID(): {span: span, context: parent},
+			},
+		}
+	}
+}
+
+func (p *Sampled) getTrace(traceID trace.TraceID) (*traceWrapper, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	tr, ok := p.traces[traceID]
+
+	return tr, ok
+}
+
+func (p *Sampled) addTrace(parent context.Context, span tracesdk.ReadWriteSpan) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	spanCtx := span.SpanContext()
+
+	tr, ok := p.traces[spanCtx.TraceID()]
+	if !ok {
+		tr = &traceWrapper{
+			parentSpanID: spanCtx.SpanID(),
+			spans:        make(map[trace.SpanID]spanWrapper),
+		}
+
+		p.traces[spanCtx.TraceID()] = tr
+	}
+
+	tr.storeSpan(parent, span)
+}
+
+func (p *Sampled) send(
+	parent context.Context,
+	span tracesdk.ReadOnlySpan,
+	hasError bool,
+) {
+	if hasError {
+		p.processor.OnEnd(span)
+	} else if sr := p.sampler.ShouldSample(tracesdk.SamplingParameters{
+		ParentContext: parent,
+		TraceID:       span.SpanContext().TraceID(),
+		Name:          span.Name(),
+		Kind:          span.SpanKind(),
+		Attributes:    span.Attributes(),
+		Links:         linksToTraceLinks(span.Links()),
+	}); sr.Decision == tracesdk.RecordAndSample {
+		p.processor.OnEnd(span)
+	}
+
+	p.removeTraces(span.SpanContext().TraceID())
+}
+
+func (p *Sampled) removeTraces(traceID trace.TraceID) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	tr, ok := p.traces[traceID]
+	if !ok {
+		return
+	}
+
+	if len(tr.spans) == 0 {
+		delete(p.traces, traceID)
+	}
+}
+
+func (p *Sampled) flush() {
+	wg := sync.WaitGroup{}
+
+	p.mu.Lock()
+
+	for _, tr := range p.traces {
+		hasError := tr.hasError()
+
+		for _, span := range tr.extractFinishedSpans() {
+			wg.Add(1)
+
+			go func(span spanWrapper) {
+				defer wg.Done()
+
+				p.send(span.context, span.span, hasError)
+			}(span)
+		}
+	}
+
+	p.mu.Unlock()
+
+	wg.Wait()
+}
+
+func linksToTraceLinks(links []tracesdk.Link) []trace.Link {
+	resp := make([]trace.Link, 0, len(links))
+
+	for _, l := range links {
+		resp = append(resp, trace.Link{
+			SpanContext: l.SpanContext,
+			Attributes:  l.Attributes,
+		})
+	}
+
+	return resp
+}
--- otel/main.go
+++ otel/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/loghole/tracing/internal/spanprocessor"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/jaeger"
@@ -29,10 +30,13 @@ func main() {
 		panic(err)
 	}
 
+	sampledProcessor := spanprocessor.NewSampled(
+		tracesdk.NewBatchSpanProcessor(exp, tracesdk.WithMaxExportBatchSize(1)),
+		tracesdk.AlwaysSample(),
+	)
+
 	tp := tracesdk.NewTracerProvider(
-		tracesdk.WithSpanProcessor(&OurSpanProcessor{
-			root: tracesdk.NewBatchSpanProcessor(exp),
-		}),
+		tracesdk.WithSpanProcessor(sampledProcessor),
 
 		// Always be sure to batch in production.
 		// tracesdk.WithBatcher(exp),
@@ -43,7 +47,6 @@ func main() {
 			attribute.String("environment", environment),
 			attribute.Int64("ID", id),
 		)),
-		tracesdk.WithSampler(&OurSampler{}),
 	)
 
 	// Register our TracerProvider as the global so any imported
@@ -55,6 +58,8 @@ func main() {
 
 	// Cleanly shutdown and flush telemetry when the application exits.
 	defer func(ctx context.Context) {
+		time.Sleep(time.Second * 10)
+
 		// Do not make the application hang when it is shutdown.
 		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
 		defer cancel()
@@ -66,7 +71,7 @@ func main() {
 	tr := tp.Tracer("component-main")
 
 	log.Println("start foo trace")
-	ctx, span := tr.Start(ctx, "foo")
+	ctx, span := tr.Start(ctx, "foo2")
 	defer span.End()
 
 	time.Sleep(time.Millisecond * 250)
@@ -76,13 +81,11 @@ func main() {
 	time.Sleep(time.Millisecond * 250)
 
 	span.AddEvent("test2", trace.WithTimestamp(time.Now()))
-	/*
-		ctx = trace.ContextWithSpanContext(
-			ctx,
-			span.SpanContext().WithTraceFlags(span.SpanContext().TraceFlags().WithSampled(false)),
-		)*/
 
-	bar(ctx, tp)
+	bar(ctx, tp, "bar1")
+
+	bar(ctx, tp, "bar2")
+	bar2(ctx, tp, "bar3")
 
 	log.Println("end")
 }
@@ -93,18 +96,39 @@ func main() {
 	ctx, span := tr.Start(ctx, "foo", trace.WithTimestamp(time.Now().Add(-5*time.Minute)))
 	defer span.End()
 */
-
-func bar(ctx context.Context, tp *tracesdk.TracerProvider) {
+func bar(ctx context.Context, tp *tracesdk.TracerProvider, name string) {
 	time.Sleep(time.Second)
 	// Use the global TracerProvider.
 	//tr := otel.Tracer("component-bar")
-	ctx, span := tp.Tracer("component-bar").Start(ctx, "bar")
+	ctx, span := tp.Tracer("component-bar").Start(ctx, name)
 	span.SetAttributes(attribute.Key("testset").String("value"))
 	defer span.End()
 
 	time.Sleep(time.Millisecond * 250)
 
-	setErrorTag(ctx)
+	//setErrorTag(ctx)
+
+	span.AddEvent("test3", trace.WithTimestamp(time.Now()))
+
+	// Do bar...
+}
+
+func bar2(ctx context.Context, tp *tracesdk.TracerProvider, name string) {
+	time.Sleep(time.Second)
+	// Use the global TracerProvider.
+	//tr := otel.Tracer("component-bar")
+	ctx, span := tp.Tracer("component-bar").Start(ctx, name)
+	span.SetAttributes(attribute.Key("testset").String("value"))
+	defer func() {
+		go func() {
+			time.Sleep(time.Second * 5)
+			span.End()
+		}()
+	}()
+
+	time.Sleep(time.Millisecond * 250)
+
+	//setErrorTag(ctx)
 
 	span.AddEvent("test3", trace.WithTimestamp(time.Now()))
 
@@ -113,112 +137,4 @@ func bar(ctx context.Context, tp *tracesdk.TracerProvider) {
 
 func setErrorTag(ctx context.Context) {
 	trace.SpanFromContext(ctx).SetAttributes(attribute.KeyValue{"error", attribute.BoolValue(true)})
-
-}
-
-type Sampler interface {
-	// DO NOT CHANGE: any modification will not be backwards compatible and
-	// must never be done outside of a new major release.
-
-	// ShouldSample returns a SamplingResult based on a decision made from the
-	// passed parameters.
-	ShouldSample(parameters tracesdk.SamplingParameters) tracesdk.SamplingResult
-	// DO NOT CHANGE: any modification will not be backwards compatible and
-	// must never be done outside of a new major release.
-
-	// Description returns information describing the Sampler.
-	Description() string
-	// DO NOT CHANGE: any modification will not be backwards compatible and
-	// must never be done outside of a new major release.
-}
-
-type OurSampler struct {
-}
-
-func (s *OurSampler) ShouldSample(parameters tracesdk.SamplingParameters) tracesdk.SamplingResult {
-	log.Println("ShouldSample")
-
-	return tracesdk.SamplingResult{
-		Decision: tracesdk.RecordAndSample,
-		Attributes: []attribute.KeyValue{
-			attribute.KeyValue{"IsRecording", attribute.BoolValue(true)},
-			attribute.KeyValue{"Sampled", attribute.BoolValue(true)},
-		},
-	}
-}
-
-func (s *OurSampler) Description() string {
-	log.Println("Description")
-
-	return ""
-}
-
-/*
-
-// ShouldSample implements the Sampler's ShouldSample().
-//nolint:gocritic
-func (rSampler *RemotelyControlledSampler) ShouldSample(param otelTrace.SamplingParameters) otelTrace.SamplingResult {
-	sampler := rSampler.getSamplerForOperation(param.Name)
-	isSampled, tags := sampler.IsSampled(param.TraceID)
-	tags = append(tags, label.Bool("IsRecording", true), label.Bool("Sampled", isSampled))
-
-	return otelTrace.SamplingResult{
-		Decision:   otelTrace.RecordAndSample,
-		Attributes: tags,
-	}
-}
-*/
-
-type SpanProcessor interface {
-
-	// OnStart is called when a span is started. It is called synchronously
-	// and should not block.
-	OnStart(parent context.Context, s tracesdk.ReadWriteSpan)
-
-	// OnEnd is called when span is finished. It is called synchronously and
-	// hence not block.
-	OnEnd(s tracesdk.ReadOnlySpan)
-
-	// Shutdown is called when the SDK shuts down. Any cleanup or release of
-	// resources held by the processor should be done in this call.
-	//
-	// Calls to OnStart, OnEnd, or ForceFlush after this has been called
-	// should be ignored.
-	//
-	// All timeouts and cancellations contained in ctx must be honored, this
-	// should not block indefinitely.
-	Shutdown(ctx context.Context) error
-
-	// ForceFlush exports all ended spans to the configured Exporter that have not yet
-	// been exported.  It should only be called when absolutely necessary, such as when
-	// using a FaaS provider that may suspend the process after an invocation, but before
-	// the Processor can export the completed spans.
-	ForceFlush(ctx context.Context) error
-}
-
-type OurSpanProcessor struct {
-	root tracesdk.SpanProcessor
-}
-
-func (p *OurSpanProcessor) OnStart(parent context.Context, s tracesdk.ReadWriteSpan) {
-	log.Printf("on start: %+v", s)
-
-	p.root.OnStart(parent, s)
-}
-
-func (p *OurSpanProcessor) OnEnd(s tracesdk.ReadOnlySpan) {
-	log.Printf("on end: %+v", s)
-
-	p.root.OnEnd(s)
-}
-
-func (p *OurSpanProcessor) Shutdown(ctx context.Context) error {
-	log.Println("on shutdown")
-
-	return p.root.Shutdown(ctx)
-}
-
-func (p *OurSpanProcessor) ForceFlush(ctx context.Context) error {
-	log.Println("on ForceFlush")
-	return p.root.ForceFlush(ctx)
 }
